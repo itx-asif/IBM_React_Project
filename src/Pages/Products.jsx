b3136e02ed21c29aa98efe096fb9fd988644e827
ASIF RAZA <158028070+itx-asif@users.noreply.github.com>
2025-03-19 23:01:11 +0500

    updated uplaod error

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import ProductList from "./productcard";
+import ProductList from "./productcards";
 
 export const Products = () => {
   const [cart, setCart] = useState({});
